a55147a5eda01053b5d64331ca6fe78edad3308a
PhyloStar <taraka@fripost.org>
2017-03-08 09:56:33 +0100

    added slider, scaler, dirichlet moves

---
+++ params_moves.py
@@ -0,0 +1,35 @@
+import numpy as np
+from scipy.stats import dirichlet
+
+dir_alpha = 100.0
+scaler_alpha = 1.25
+
+def mvDirichlet(pi):
+    pi_new = np.random.dirichlet(dir_alpha*pi)
+    hastings_ratio = dirichlet.logpdf(pi, pi_new) - dirichlet.logpdf(pi_new, pi)
+    return pi_new, hastings_ratio
+
+def mvDualSlider(pi):
+    i,j = np.sample(len(range(len(pi))),2)
+    sum_ij = sum(pi[i]+pi[j])
+    x = np.random.uniform(sum_ij)
+    y = sum_ij -x
+    pi[i], pi[j] = x, y
+    return pi
+
+def multiplier(x):
+    log_c = scaler_alpha*(np.random.uniform()-0.5)
+    c = np.exp(log_c)
+    x_new = x*c
+    return x_new, log_c
+
+def slider(x, a, b):
+    """ a and b are bounds
+    """
+    x_hat = np.random.uniform(x-0.5, x+0.5)
+    if x_hat < a:
+        return 2.0*a -x_hat
+    elif xhat > b:
+        return 2.0*b -x_hat
+    else:
+        return x_hat 
