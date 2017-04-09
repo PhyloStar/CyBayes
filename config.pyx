d5fd1e74424eed50e8a1ab540eade91b23900315
taraka_rama <tarakaramadaiict@gmail.com>
2017-04-09 11:27:54 +0200

    added config file

--- config.pyx
+++ config.pyx
@@ -3,3 +3,19 @@ cdef list ALPHABET, TAXA
 cdef double NORM_BETA
 cdef dict LEAF_LLMAT
 cdef str MODEL, IN_DTYPE
+
+N_CHARS = 0
+N_TAXA = 0
+N_SITES = 0
+N_GEN = 0
+THIN = 0
+
+ALPHABET = []
+TAXA = []
+
+NORM_BETA = 0.0
+
+LEAF_LLMAT = {}
+
+MODEL = ""
+IN_DTYPE = ""
